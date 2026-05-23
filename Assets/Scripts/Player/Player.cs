af1f8cac69878536bd0ddde0faa122a5ba82a943
dafral <dafral1996@gmail.com>
2026-05-23 21:33:50 +0200

    Player basic movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,24 +1,33 @@
 using Dafral.Grid;
+using Dafral.Services;
 using UnityEngine;
 
 namespace Dafral.Player
 {
-    public class Player : MonoBehaviour, IPlayer
+    public class Player : MonoBehaviour, IPlayer, IGridEntity
     {
         [SerializeField] private PlayerMovement _playerMovement;
+        private Vector2Int _gridPosition;
 
-        public Vector2Int GridPosition => throw new System.NotImplementedException();
+        public Vector2Int GridPosition => _gridPosition;
+        public bool IsMoving => _playerMovement.IsMoving;
 
-        public bool IsMoving => throw new System.NotImplementedException();
+        private void Start()
+        {
+            var gridService = ServiceLocator.Instance.GetService<IGridService>();
+            _gridPosition = gridService.GetGridPosition(transform.position);
+            gridService.TryPlaceEntity(this, _gridPosition);
+            _playerMovement.Initialize(this, transform);
+        }
 
         public void SetGridPosition(Vector2Int position)
         {
-            throw new System.NotImplementedException();
+            _gridPosition = position;
         }
 
         public bool TryMove(Vector2Int direction)
         {
-            throw new System.NotImplementedException();
+            return _playerMovement.TryToMove(direction);
         }
     }
 }
