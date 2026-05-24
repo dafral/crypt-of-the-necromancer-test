39ae8629cf825b33a8dad9ca1e303124aef16baa
dafral <dafral1996@gmail.com>
2026-05-24 12:34:14 +0200

    UIService

--- Assets/Scripts/Services/ServiceInstaller.cs
+++ Assets/Scripts/Services/ServiceInstaller.cs
@@ -4,10 +4,11 @@ namespace Dafral.Services
 {
     public class ServiceInstaller
     {
-        public void Install()
+        public void Install(UIConfiguration uiConfiguration)
         {
             var eventService = ServiceLocator.Instance.RegisterService<IEventService>(new EventService());
             ServiceLocator.Instance.RegisterService<IInputService>(new InputService(eventService));
+            ServiceLocator.Instance.RegisterService<IUIService>(new UIService(uiConfiguration));
             ServiceLocator.Instance.RegisterService<IGridService>(new GridService());
         }
     }
